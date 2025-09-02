d17e255e6eb498c31fe2a163cdb31eed1e70d109
edmilsonmedeiros <edmilson.mprojetos@gmail.com>
2025-09-02 00:14:15 -0300

    First commit

--- routes/admin.php
+++ routes/admin.php
@@ -6,8 +6,6 @@ use App\Http\Controllers\ResumeController;
 Route::group(['middleware' => ['auth']], function () {
     Route::prefix('admin')->group(function () {
         Route::post('/get-resumes', [ResumeController::class, 'getResumes'])->name('admin.get-resumes');
-        Route::get('/download-resume/{id}', [ResumeController::class, 'downloadResume'])->name('admin.download-resume');
-
         Route::get('download-resume/{id}', [ResumeController::class, 'downloadResume'])->name('admin.download-resume');
     });
 });
